82d1573ffef24969393663c71ab08c3f53dec0cf
Samthibau <Samthibault28@gmail.com>
2024-12-15 16:51:04 -0600

    add Svelte

---
+++ .svelte-kit/generated/client/nodes/3.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/launches/+page.svelte";
---
+++ .svelte-kit/generated/client/nodes/6.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/pages/labs/+page.svelte";
